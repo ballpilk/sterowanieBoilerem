965d658f9d1d9955a812c93e2f6261f7a54ef679
ballpilk <ballpilk@gmail.com>
2017-10-28 23:25:37 +0200

    file moved

--- src/sketch_nov02a.ino
+++ src/sketch_nov02a.ino
@@ -11,8 +11,10 @@
 // Adres czujnika
 byte address[8] = {0x28, 0x20, 0xF8, 0x0, 0x8, 0x0, 0x0, 0x15};
 int ledPin = 13;
-int histereza = 7;
+int histereza = 5;
 int start = 40;
+bool stateOn = false;
+
 OneWire onewire(ONEWIRE_PIN);
 DS18B20 sensors(&onewire);
 class Controller
@@ -21,13 +23,19 @@ class Controller
   void updateTemp(float temp)
   {
     if(temp >=start+histereza)
+    {
       digitalWrite(ledPin, LOW);
+      stateOn = true;
+    }
     else if(temp < start)
+    {
         digitalWrite(ledPin, HIGH);
+        stateOn = false;
+    }
   }
  };
 
- Controller controller;
+Controller controller;
 long unsigned int timestamp(0);
 void setup() {
   while(!Serial);
@@ -44,7 +52,8 @@ void loop() {
     float temperature = sensors.readTemperature(address);
 
     Serial.print(temperature);
-    Serial.println(F(" 'C"));
+    Serial.print(F(" 'C "));
+    Serial.println(stateOn ? "  ON": " OFF");
     if( millis() - timestamp > 1000)
     {
       timestamp = millis();
@@ -52,7 +61,10 @@ void loop() {
     }
     sensors.request(address);
   }
+if(Serial.available() > 0)
+{
+  start = Serial.parseInt();
+  histereza = Serial.parseInt();
+}
 
-  // tu umieść resztę twojego programu
-  // Będzie działał bez blokowania
 }
